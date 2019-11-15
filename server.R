129c42032aae18ef24894313f28aa1e83d71e6a4
burkj2003 <32002619+burkj2003@users.noreply.github.com>
2019-11-15 02:21:08 -0600

    a

--- server.R
+++ server.R
@@ -1,13 +1,63 @@
 server <- function(input, output, session) {
-  dataset <- reactive({
-    get(input$dataset, "package:datasets")
+  
+  
+  # Generate a plot of the data ----
+  # Also uses the inputs to build the plot label. Note that the
+  # dependencies on the inputs and the data reactive expression are
+  # both tracked, and all expressions are called in the sequence
+  # implied by the dependency graph.
+  output$plot <- renderPlot({
+    
+    
+    req(input$file1)
+    
+    nona <- read.csv(input$file1$datapath,header=T)
+    
+    if (input$radio == 1 & input$radio2 == 1) {
+      hist(nona$IBU, breaks = input$bins)
+    }
+      else {
+        if (input$radio == 2 & input$radio2 == 1) {
+          hist(nona$ABV, breaks = input$bins)
+        }
+        else {
+          if (input$radio == 1 & input$radio2 == 2) {
+            boxplot(nona$IBU)
+          }
+          else {
+            boxplot(nona$ABV)
+          }
+        }
+    }
+
+    dataset <- reactive({
+      # Make sure requirements are met
+      req(input$state)
+      
+      get(inpsut$state, "package:datasets", inherits = FALSE)
+    })
+
+    br()
+  
+
+  
+    # dist <- input$dist
+    # bins <- input$bins
+    # 
+    # hist(d(),
+    #      main = paste("r", dist, "(", bins, ")", sep = ""),
+    #      col = "#75AADB", border = "white")
   })
   
+  
+  # Generate a summary of the data ----
   output$summary <- renderPrint({
-    summary(dataset())
+    summary(d())
   })
   
+  # Generate an HTML table view of the data ----
   output$table <- renderTable({
-    dataset()
+    d()
   })
+  
 }
--- ui.R
+++ ui.R
@@ -27,17 +27,41 @@ ui <- fluidPage(
                    choices = list("IBU" = 1, "ABV" = 2), 
                    selected = 1),
       
+      
+      # Copy the line below to make a set of radio buttons
+      radioButtons("radio2", label = h3("Histogram or BoxPlot"),
+                   choices = list("Hist" = 1, "Box" = 2), 
+                   selected = 1),
+      
+    
+      br(),
+      
+      selectizeInput(
+        "state", "Enter a state or state(s)", choices = state.name, multiple = TRUE
+      ),
+      
+      # Input: Slider for the number of observations to generate ----
+      sliderInput(inputId = "bins",
+                  label = "Number of bins for histogram:",
+                  value = 100,
+                  min = 1,
+                  max = 200),
+      
+        
         ),
     
-    
+
     
     # Main panel for displaying outputs ----
     mainPanel(
       
       # Output: Data file ----
       #tableOutput(outputId = "table")
-      plotOutput("plotoutput"),
-      tableOutput('tableoutput')
+      tabsetPanel(type = "tabs",
+                  tabPanel("Plot", plotOutput("plot"))
+                  # tabPanel("Summary", verbatimTextOutput("summary")),
+                  # tabPanel("Table", tableOutput("table"))
+      )
     )
     
   )
